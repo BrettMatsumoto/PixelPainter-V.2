eab3254ce9d4a89a2c4592e88b38ebf8e3b2bf10
brett <brett_matsumoto@yahoo.com>
2020-02-29 08:59:20 -1000

    tested boiler plate code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 'use strict';
+
+let mainCanvas = document.createElement('div');
+mainCanvas.id = 'mainCanvas';
+document.body.appendChild(mainCanvas);
+
+let pixelMatrix = [];
+let rowCount = 10;
+let colCount = 10;
+
+const makeCanvasGrid = (x, y) => {
+    for (let i = 0; i < y; i++) {
+        let column = document.createElement('div');
+        let columArr = [];
+        column.className = 'gridColumn';
+        mainCanvas.appendChild(column);
+        for (let j = 0; j < x; j++) {
+            let row = document.createElement('div');
+            row.className = 'gridRow';
+            column.appendChild(row);
+            columArr.push(row);
+        }
+    }
+    pixelMatrix.push(columArr);
+}
+makeCanvasGrid(numRows, numColumns);
